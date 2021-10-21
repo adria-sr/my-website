8003c4281c5c3a83617fa89b91f90c504efbf591
adria-sr <adria.soubias@orange.fr>
2021-10-21 14:16:14 +0200

    uptated files

--- js/espace-sensible.js
+++ js/espace-sensible.js
@@ -1,3 +1,4 @@
+// Detect request animation frame
 var scroll = window.requestAnimationFrame ||
   // IE Fallback
   function(callback) {
@@ -7,13 +8,11 @@ var elementsToShow = document.querySelectorAll('.show-on-scroll');
 
 function loop() {
 
-  Array.prototype.forEach.call(elementsToShow, function(element) {
+  elementsToShow.forEach(function(element) {
     if (isElementInViewport(element)) {
       element.classList.add('is-visible');
-      console.log('added is-visible')
     } else {
       element.classList.remove('is-visible');
-      console.log('removed is-visible');
     }
   });
 
@@ -23,15 +22,19 @@ function loop() {
 // Call the loop for the first time
 loop();
 
-const callback = function(entries) {
-  entries.forEach(entry => {
-    entry.target.classList.toggle("is-visible");
-  });
-};
-
-const observer = new IntersectionObserver(callback);
-
-const targets = document.querySelectorAll(".show-on-scroll");
-targets.forEach(function(target) {
-  observer.observe(target);
-});
+// Helper function from: http://stackoverflow.com/a/7557433/274826
+function isElementInViewport(el) {
+  // special bonus for those using jQuery
+  if (typeof jQuery === "function" && el instanceof jQuery) {
+    el = el[0];
+  }
+  var rect = el.getBoundingClientRect();
+  return (
+    (rect.top <= 0 &&
+      rect.bottom >= 0) ||
+    (rect.bottom >= (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.top <= (window.innerHeight || document.documentElement.clientHeight)) ||
+    (rect.top >= 0 &&
+      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight))
+  );
+}
