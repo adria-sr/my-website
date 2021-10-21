53da5ea4d3d9d7001486f39f3b82d08a3eb7dcba
adria-sr <adria.soubias@orange.fr>
2021-10-21 14:30:28 +0200

    worked on js

--- js/espace-sensible.js
+++ js/espace-sensible.js
@@ -4,7 +4,8 @@ var scroll = window.requestAnimationFrame ||
   function(callback) {
     window.setTimeout(callback, 1000 / 60)
   };
-var elementsToShow = document.querySelectorAll('.show-on-scroll');
+
+var elementsToShow = document.querySelectorAll(".show-on-scroll");
 
 function loop() {
 
