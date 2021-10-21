378cb1822a6ddfa4553f00be08dc37d6d36d6945
adria-sr <adria.soubias@orange.fr>
2021-10-21 15:26:21 +0200

    add css animation

--- js/espace-sensible.js
+++ js/espace-sensible.js
@@ -1,41 +1,15 @@
-// Detect request animation frame
-var scroll = window.requestAnimationFrame ||
-  // IE Fallback
-  function(callback) {
-    window.setTimeout(callback, 1000 / 60)
-  };
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    const square = entry.target.querySelector('.square');
 
-var elementsToShow = document.querySelectorAll(".show-on-scroll");
-
-function loop() {
-
-  elementsToShow.forEach(function(element) {
-    if (isElementInViewport(element)) {
-      element.classList.add('is-visible');
-    } else {
-      element.classList.remove('is-visible');
+    if (entry.isIntersecting) {
+      square.classList.add('square-animation');
+      return; // if we added the class, exit the function
     }
+
+    // We're not intersecting, so remove the class!
+    square.classList.remove('square-animation');
   });
+});
 
-  scroll(loop);
-}
-
-// Call the loop for the first time
-loop();
-
-// Helper function from: http://stackoverflow.com/a/7557433/274826
-function isElementInViewport(el) {
-  // special bonus for those using jQuery
-  if (typeof jQuery === "function" && el instanceof jQuery) {
-    el = el[0];
-  }
-  var rect = el.getBoundingClientRect();
-  return (
-    (rect.top <= 0 &&
-      rect.bottom >= 0) ||
-    (rect.bottom >= (window.innerHeight || document.documentElement.clientHeight) &&
-      rect.top <= (window.innerHeight || document.documentElement.clientHeight)) ||
-    (rect.top >= 0 &&
-      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight))
-  );
-}
+observer.observe(document.querySelector('.square-wrapper'));
