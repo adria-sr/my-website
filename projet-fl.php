2b66442e338ca49dd09f643e2553d4b7a9a2d8e7
Adrià <78359397+adria-sr@users.noreply.github.com>
2023-12-19 16:56:57 +0100

    Rework top project page

--- projet-fl.php
+++ projet-fl.php
@@ -3,30 +3,29 @@
 <div class="container-fluid">
 
   <div class="container-fluid p-4">
-
+<div class="row">
+<div class="col-lg-4">
+  <h1>Place Fontaine Lestang</h1>
+  <p class="mb-4">2023 · Projet de diplôme · Édition imprimée · Méthodologie participative</p>
+<h2>      Concevoir un écosystème graphique pour la place Fontaine Lestang, avec les habitants et usagers du lieu. <br><br></h2>
+</div>
+</div>
     <div class="row">
-      <div class="col-lg-4 mb-4">
-          <h1>Place Fontaine Lestang</h1>
+      <div class="col-lg-4">
+
         <h2>
-
-          Concevoir un écosystème graphique pour la place Fontaine Lestang, avec les habitants et usagers du lieu. <br><br>
-
   Un écosystème graphique peut se définir comme "les outils et les règles graphiques qui garantissent la composition cohérente et ordonnée du nouveau paysage" mais il est aussi une "base ouverte qui se prête à être complétée et enrichie avec de nouvelles séries et développements graphiques." (Leku Studio). La notion d'écosystème induit aussi des interactions, des échanges et s'oppose au un kit graphique en s'inscrivant dans un contexte précis, un réalité tangible qui, dans ce cas, est la place Fontaine Lestang.<br><br>
 
   Le développement de l'écosystème se déroule en trois temps/ateliers. Un premier atelier in situ permet de faire connaitre le projet et de faire découvrir au plus grand nombre toutes les possibilités d'un tel dispositif au travers une sélection d'exemples d'installation graphiques déjà existantes. Un deuxième atelier en salle permet aux participants de faire de leurs propres mains en dessinant sur un fond de carte à partir de formes prédéfinies afin de mettre en valeur les espaces les plus délaissés de la place. Enfin, le troisième atelier s'organise comme une restitution de toutes les étapes du projet. Lors de ce dernier temps, trois propositions d'écosystème graphique sont soumises au vote. Elles sont réalisées par le designer graphique à l'issue du deuxième atelier et sont pensées comme une synthèse de toutes les participations. Enfin, en fonction des retours, un dernier retravail de la proposition sélectionnée est réalisé avant l'installation de l'écosystème graphique.<br><br>
 
-  Au fur et à mesure de l'avancement du projet, les supports de communication se teintent de la participation des habitants : l'identité visuelle "rubalise", avec les formes de sa grille et sa tonique orange est déconstruite et réappropriée par les participants qui jouissent d'une liberté totale dans l'utilisation des formes proposées dans le deuxième atelier.<br><br>
-
+  Au fur et à mesure de l'avancement du projet, les supports de communication se teintent de la participation des habitants : l'identité visuelle "rubalise", avec les formes de sa grille et sa tonique orange est déconstruite et réappropriée par les participants qui jouissent d'une liberté totale dans l'utilisation des formes proposées dans le deuxième atelier.
         </h2>
       </div>
-      <div class="col-lg-6">
-        <ul>
-          <li>2023</li>
-          <li>Projet de diplôme</li>
-          <li></li>
-        </ul>
-      </div>
-      <div class="col">
+      <div class="col-lg">
+    <figure>
+      <img class="img-fluid" src="img\fl-affiche.jpg" alt="">
+<figcaption class="mt- text-end">Affiche d'avant projet annonçant le début de la consultation <i>in situ</i>.</figcaption>
+    </figure>
       </div>
     </div>
   </div>
@@ -34,15 +33,14 @@
 
 <div class="container-fluid p-4">
   <div class="row g-0">
-    <div class="col-lg-4 mb-4">
-      <img class="img-fluid" src="img\fl-affiche.jpg" alt="">
-    </div>
-  </div>
-  <div class="row g-0">
     <div class="col-lg-4">
     </div>
     <div class="col-lg mb-4">
-      <img class="img-fluid" src="img\fl-drapeau.jpg" alt="">
+      <figure>
+        <img class="img-fluid" src="img\fl-drapeau.jpg" alt="Drapeau vertical orange signalant le projet sur la place">
+
+        <figcaption class="mt- text-end" >Drapeau signalant le projet sur la place.</figcaption>
+      </figure>
     </div>
       </div>
     <div class="row g-0">
@@ -52,7 +50,7 @@
         <img class="img-fluid" src="img\fl-fiches.jpg" alt="">
       </div>
       <div class="row g-0">
-        <div class="col-lg-4 mb-4">
+        <div class="col-lg-5 mb-4">
           <img class="img-fluid" src="img\fl-restitution.jpg" alt="">
         </div>
         <div class="col-lg">
@@ -60,7 +58,7 @@
         </div>
   </div>
   <div class="row g-0">
-    <div class="col-lg-4 mb-4">
+    <div class="col-lg-5 mb-4">
       <img class="img-fluid" src="img\fl-tampon.jpg" alt="">
     </div>
     <div class="col-lg">
@@ -82,12 +80,14 @@
     </div>
   </div>
   <div class="row g-0">
-    <div class="col-lg-4 mb-4">
+    <div class="col-lg-5 mb-4">
       <img class="img-fluid" src="img\fl-mockup.jpg" alt="">
     </div>
     <div class="col-lg">
     </div>
   </div>
+</div>
+
 
 
   <?php echo file_get_contents("footer.html"); ?>
